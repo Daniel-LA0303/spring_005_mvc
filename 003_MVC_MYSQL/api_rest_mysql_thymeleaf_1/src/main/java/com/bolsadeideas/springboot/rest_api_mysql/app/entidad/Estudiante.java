6f154c3424fd2d939097a2b49f02bc4084feba3f
daniel-la <03daniel030399@gmail.com>
2025-08-04 19:13:35 -0600

    add changes

--- 003_MVC_MYSQL/api_rest_mysql_thymeleaf_1/src/main/java/com/bolsadeideas/springboot/rest_api_mysql/app/entidad/Estudiante.java
+++ 003_MVC_MYSQL/api_rest_mysql_thymeleaf_1/src/main/java/com/bolsadeideas/springboot/rest_api_mysql/app/entidad/Estudiante.java
@@ -1,6 +1,5 @@
 package com.bolsadeideas.springboot.rest_api_mysql.app.entidad;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,24 +8,24 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "estudiantes", catalog = "spring_002_db_mvc_thymeleaf", schema = "")
+@Table(name = "estudiantes", catalog = "spring_007_db_mvc_thymeleaf", schema = "")
 public class Estudiante {
 	@Id
 	@Column
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "nombre", nullable = false, length = 50)
 	private String nombre;
-	
+
 	@Column(name = "apellido", nullable = false, length = 50)
 	private String apellido;
-	
+
 	@Column(name = "email", nullable = false, length = 50, unique = true)
 	private String email;
-	
+
 	public Estudiante() {
-		
+
 	}
 
 	public Estudiante(Long id, String nombre, String apellido, String email) {
@@ -36,7 +35,7 @@ public class Estudiante {
 		this.apellido = apellido;
 		this.email = email;
 	}
-	
+
 	public Estudiante(String nombre, String apellido, String email) {
 		super();
 		this.nombre = nombre;
@@ -44,43 +43,41 @@ public class Estudiante {
 		this.email = email;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
 	public String getApellido() {
 		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	@Override
 	public String toString() {
 		return "Estudiante [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + "]";
 	}
-	
 
-	
 }
--- 004_MVC_MONGO/api_rest_mongo_thymeleaf/src/main/java/com/bolsadeideas/springboot/rest_api_mongo/app/ApiRestMongoThymeleafApplication.java
+++ 004_MVC_MONGO/api_rest_mongo_thymeleaf/src/main/java/com/bolsadeideas/springboot/rest_api_mongo/app/ApiRestMongoThymeleafApplication.java
@@ -3,10 +3,16 @@ package com.bolsadeideas.springboot.rest_api_mongo.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ApiRestMongoThymeleafApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+
 		SpringApplication.run(ApiRestMongoThymeleafApplication.class, args);
 	}
 
